2409c96725504604e030ddccf243f8d1b3137fdb
Thanh Duong <dhthanhqa@gmail.com>
2020-11-03 14:58:35 +0700

    start project

---
+++ MulipleChoiceAppV1/src/services/CategoryServices.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import pojo.Category;
+
+/**
+ *
+ * @author Admin
+ */
+public class CategoryServices {
+    public static List<Category> readCategories() 
+            throws ClassNotFoundException, SQLException {
+        // Nạp Driver
+        Class.forName("com.mysql.jdbc.Driver");
+        
+        // Kết nối CSDL
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost/englishdb", 
+                "root", "123456");
+        String sql = "SELECT * FROM category";
+        Statement stm = conn.createStatement();
+        ResultSet rs = stm.executeQuery(sql);
+        
+        List<Category> kq = new ArrayList<>();
+        while (rs.next()) {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            Category c = new Category(id, name);
+            kq.add(c);
+        }
+        
+        return kq;
+    }
+}
